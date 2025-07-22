f7e19c1b4d57bd774deab422f22950076243e70f
nicoCorbal <nicocorbal@gmail.com>
2025-07-22 22:27:46 +0200

    Icons improvement

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
+import lucide from 'lucide-astro';
 
 export default defineConfig({
   output: 'static',
-  integrations: [tailwind(), react()],
+  integrations: [tailwind(), react(), lucide()],
 });
